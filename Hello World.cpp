68af3258329023bfba02bc5f947de3f02727200d
gulamnabifiroz <khangulam2005@gmail.com>
2024-10-03 11:55:07 +0530

    Add files via upload

---
+++ Hello World.cpp
@@ -0,0 +1,12 @@
+// HelloWorld.cpp
+//Gulamnabi Khan
+//ENTC A3
+//23070123503
+#include <iostream>
+using namespace std;
+int main() {
+    cout << "Hello, World!" << endl; 
+    return 0;
+}
+//Output
+//Hello, World!
---
+++ Simple Calculator.cpp
@@ -0,0 +1,29 @@
+// Name -Gulamnabi Khan
+// PRN - 23070123503
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    float num1, num2;
+    cout << "Please enter the first number: ";
+    cin >> num1;
+    cout << "Please enter the second number: ";
+    cin >> num2;
+    cout << "The sum of " << num1 << " and " << num2 << " is: " << num1 + num2 << "\n";
+    cout << "The difference when subtracting " << num2 << " from " << num1 << " is: " << num1 - num2 << "\n";
+    cout << "The result of dividing " << num1 << " by " << num2 << " is: " << num1 / num2 << "\n";
+    cout << "The product of " << num1 << " and " << num2 << " is: " << num1 * num2 << "\n";
+
+    return 0;
+}
+
+/*
+Output:
+Please enter the first number: 5
+Please enter the second number: 6
+The sum of 5 and 6 is: 11
+The difference when subtracting 6 from 5 is: -1
+The result of dividing 5 by 6 is: 0.833333
+The product of 5 and 6 is: 30
+*/
